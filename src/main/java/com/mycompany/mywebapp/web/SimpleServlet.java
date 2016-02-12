def73602fd53e39d7cb7c1bdd674eb85fcc691e9
Kabir Mahmood <kabir.mahmood@endava.com>
2016-02-12 15:05:11 +0000

    v8.7

--- src/main/java/com/mycompany/mywebapp/web/SimpleServlet.java
+++ src/main/java/com/mycompany/mywebapp/web/SimpleServlet.java
@@ -14,7 +14,7 @@ public class SimpleServlet extends HttpServlet {
         Integer sum = Integer.parseInt(a) + Integer.parseInt(b);
 
         PrintWriter out = response.getWriter();
-        out.println( "<h1>SimpleServlet v8.6 - The sum of " + a.toString() + " + " + b.toString() + " = " + sum + "</h2>" );
+        out.println( "<h1>SimpleServlet v8.7 - The sum of " + a.toString() + " + " + b.toString() + " = " + sum + "</h2>" );
         out.flush();
         out.close();
     }
